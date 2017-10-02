6d2c49a18db5c3d24db58e541fa5d0d699b99af4
Ryan Leach <ryan.n.leach@gmail.com>
2017-10-01 23:50:41 -0600

    Added isentrops.

--- src/sounding.rs
+++ src/sounding.rs
@@ -144,11 +144,21 @@ fn draw_sounding(sounding_area: &DrawingArea, cr: &Context, sc: SoundingContextP
         y0: 1.0,
     });
 
-
     // Draw black backgound
     cr.rectangle(0.0, 0.0, aspect_ratio, 1.0);
     cr.set_source_rgb(0.0, 0.0, 0.0);
     cr.fill();
+
+    // Draw isentrops
+    for theta in &ISENTROPS {
+        plot_curve_from_points(
+            cr,
+            &sc,
+            1.0,
+            (0.6, 0.6, 0.0, 0.5),
+            generate_isentrop(*theta),
+        );
+    }
 
     // Draw blue lines below freezing
     let mut end_points: Vec<_> = COLD_ISOTHERMS
@@ -169,7 +179,7 @@ fn draw_sounding(sounding_area: &DrawingArea, cr: &Context, sc: SoundingContextP
     plot_straight_lines(cr, &sc, 1.0, (1.0, 0.0, 0.0, 0.5), &end_points);
 
     // Draw pressure lines
-    end_points = ISO_BARS
+    end_points = ISOBARS
         .into_iter()
         .map(|p| ((-150.0, *p), (60.0, *p)))
         .collect();
@@ -195,6 +205,46 @@ fn plot_straight_lines(
         cr.line_to(end.0, end.1);
     }
     cr.stroke();
+}
+
+fn plot_curve_from_points(
+    cr: &Context,
+    sc: &SoundingContext,
+    line_width_pixels: f64,
+    rgba: (f64, f64, f64, f64),
+    points: Vec<TPCoords>,
+) {
+    cr.set_source_rgba(rgba.0, rgba.1, rgba.2, rgba.3);
+    cr.set_line_width(cr.device_to_user_distance(line_width_pixels, 0.0).0);
+
+    let mut iter = points.into_iter();
+    let start = sc.convert_tp_to_screen(iter.by_ref().next().unwrap());
+    cr.move_to(start.0, start.1);
+    for end in iter {
+        let end = sc.convert_tp_to_screen(end);
+        cr.line_to(end.0, end.1);
+    }
+
+    cr.stroke();
+}
+
+fn generate_isentrop(theta: f32) -> Vec<TPCoords> {
+    use std::f32;
+    const P0: f32 = 1000.0; // For calcuating theta
+    const PS: f32 = SoundingContext::MAXP; // This and the next one are the range to calc for.
+    const PF: f32 = 300.0;
+    const NUM_PNTS: u32 = 30;
+
+    let mut result = vec![];
+
+    let mut p = SoundingContext::MAXP;
+    while p >= 300.0 {
+        let t = theta * f32::powf(P0 / p, -0.286) - 273.15;
+        result.push((t, p));
+        p += (PF - PS) / (NUM_PNTS as f32);
+    }
+
+    result
 }
 
 const COLD_ISOTHERMS: [f32; 19] = [
@@ -234,7 +284,7 @@ const WARM_ISOTHERMS: [f32; 12] = [
     60.0,
 ];
 
-const ISO_BARS: [f32; 9] = [
+const ISOBARS: [f32; 9] = [
     1050.0,
     1000.0,
     925.0,
@@ -244,4 +294,24 @@ const ISO_BARS: [f32; 9] = [
     300.0,
     200.0,
     100.0,
+];
+
+const ISENTROPS: [f32; 17] = [
+    230.0,
+    240.0,
+    250.0,
+    260.0,
+    270.0,
+    280.0,
+    290.0,
+    300.0,
+    310.0,
+    320.0,
+    330.0,
+    340.0,
+    350.0,
+    360.0,
+    370.0,
+    380.0,
+    390.0,
 ];
