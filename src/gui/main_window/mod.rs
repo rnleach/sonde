65367f62bdb3727daaf52c433f24b75d9fb4275e
Ryan Leach <ryan.n.leach@gmail.com>
2017-10-25 20:02:59 -0600

    Hide other panels for now.

--- src/gui/main_window/mod.rs
+++ src/gui/main_window/mod.rs
@@ -71,10 +71,10 @@ fn layout_drawing_areas(gui: &Gui) -> gtk::Grid {
 
     let grid = gtk::Grid::new();
     grid.attach(&add_border_frame(&gui.get_sounding_area()), 0, 0, 2, 3);
-    grid.attach(&add_border_frame(&gui.get_hodograph_area()), 2, 0, 1, 1);
-    let (ia1, ia2) = gui.get_index_areas();
-    grid.attach(&add_border_frame(&ia1), 2, 1, 1, 2);
-    grid.attach(&add_border_frame(&ia2), 0, 3, 3, 1);
+    // grid.attach(&add_border_frame(&gui.get_hodograph_area()), 2, 0, 1, 1);
+    // let (ia1, ia2) = gui.get_index_areas();
+    // grid.attach(&add_border_frame(&ia1), 2, 1, 1, 2);
+    // grid.attach(&add_border_frame(&ia2), 0, 3, 3, 1);
 
     grid
 }
