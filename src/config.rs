36cdba84aa67547b06381daa82d1d9238e3ccaea
Ryan Leach <ryan.n.leach@gmail.com>
2017-10-31 20:01:23 -0600

    This closes #4.

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,7 @@
 //! Keep configuration data in this module.
 
+use coords::TPCoords;
+
 // TODO: Organize this better
 
 // Layout and borders
@@ -48,6 +50,13 @@ pub const MINP: f64 = 99.0; // mb
 pub const MINT: f64 = -46.5; // C - at MAXP
 /// Warmest temperature plotted at max pressure, on the bottom edge.
 pub const MAXT: f64 = 50.5; // C - at MAXP
+/// Hightest elevation pressure level to draw isentrops up to
+pub const ISENTROPS_TOP_P: f64 = 200.0;
+/// Number of points to use per isentrop line when drawing.
+pub const POINTS_PER_ISENTROP: u32 = 30;
+/// Hightest elevation pressure level to draw iso mixing ratio up to
+pub const ISO_MIXING_RATIO_TOP_P: f64 = 300.0;
+
 
 /// Line width in pixels for skew-t background lines.
 pub const BACKGROUND_LINE_WIDTH: f64 = 1.0;
@@ -132,8 +141,194 @@ pub const ISOBARS: [f64; 9] = [
     100.0,
 ];
 
+/// Isentrops to plot on the chart background.
+pub const ISENTROPS: [f64; 17] = [
+    230.0,
+    240.0,
+    250.0,
+    260.0,
+    270.0,
+    280.0,
+    290.0,
+    300.0,
+    310.0,
+    320.0,
+    330.0,
+    340.0,
+    350.0,
+    360.0,
+    370.0,
+    380.0,
+    390.0,
+];
+
+/// Constant theta-e in Celsius.
+pub const ISO_THETA_E_C: [f64; 31] = [
+    -20.0,
+    -18.0,
+    -16.0,
+    -14.0,
+    -12.0,
+    -10.0,
+    -8.0,
+    -6.0,
+    -4.0,
+    -2.0,
+    0.0,
+    2.0,
+    4.0,
+    6.0,
+    8.0,
+    10.0,
+    12.0,
+    14.0,
+    16.0,
+    18.0,
+    20.0,
+    22.0,
+    24.0,
+    26.0,
+    28.0,
+    30.0,
+    32.0,
+    34.0,
+    36.0,
+    38.0,
+    40.0,
+];
+
+/// Isopleths of mixing ratio
+pub const ISO_MIXING_RATIO: [f64; 32] = [
+    0.1,
+    0.2,
+    0.4,
+    0.6,
+    0.8,
+    1.0,
+    1.5,
+    2.0,
+    2.5,
+    3.0,
+    4.0,
+    5.0,
+    6.0,
+    7.0,
+    8.0,
+    10.0,
+    12.0,
+    14.0,
+    16.0,
+    18.0,
+    20.0,
+    24.0,
+    28.0,
+    32.0,
+    36.0,
+    40.0,
+    44.0,
+    48.0,
+    52.0,
+    56.0,
+    60.0,
+    68.0,
+//    76.0, // Uncomment this when we can have arrays larger than 32.
+];
+
 /* ------------------------------------------------------------------------------------------------
 Values below this line are automatically calculated based on the configuration values above and
 should not be altered.
 ------------------------------------------------------------------------------------------------ */
-include!("background_data.rs");
+
+lazy_static! {
+
+    /// Compute points for background isotherms only once
+    pub static ref ISOTHERM_PNTS: Vec<(TPCoords, TPCoords)> = {
+        ISOTHERMS
+        .into_iter()
+        .map(|t| (TPCoords{temperature:*t, pressure:MAXP}, TPCoords{temperature:*t, pressure:MINP}))
+        .collect()
+    };
+
+    /// Compute points for background isobars only once
+    pub static ref ISOBAR_PNTS: Vec<(TPCoords, TPCoords)> = {
+        ISOBARS
+            .into_iter()
+            .map(|p| (TPCoords{temperature:-150.0, pressure:*p}, TPCoords{temperature:60.0, pressure:*p}))
+            .collect()
+    };
+
+    /// Compute points for background isentrops only once
+    pub static ref ISENTROP_PNTS: Vec<Vec<TPCoords>> = {
+        ISENTROPS
+        .into_iter()
+        .map(|theta| generate_isentrop(*theta))
+        .collect()
+    };
+
+    /// Compute points for background mixing ratio only once
+    pub static ref ISO_MIXING_RATIO_PNTS: Vec<(TPCoords, TPCoords)> = {
+        ISO_MIXING_RATIO
+        .into_iter()
+        .map(|mw| {
+            (
+                TPCoords{
+                    temperature: ::formula::temperature_from_p_and_saturated_mw(MAXP, *mw),
+                    pressure: MAXP
+                },
+                TPCoords{
+                    temperature: ::formula::temperature_from_p_and_saturated_mw(ISO_MIXING_RATIO_TOP_P, *mw),
+                    pressure: ISO_MIXING_RATIO_TOP_P,
+                },
+            )
+        })
+        .collect()
+    };
+
+    /// Compute points for background theta-e
+    pub static ref ISO_THETA_E_PNTS: Vec<Vec<TPCoords>> = {
+        use formula::{find_root, theta_e_saturated_kelvin};
+
+        ISO_THETA_E_C
+        .iter()
+        .map(|theta_c| theta_e_saturated_kelvin(1000.0, *theta_c))
+        .map(|theta_e_k| {
+            let mut v = vec![];
+            let mut p = ISENTROPS_TOP_P;
+            let dp = (MAXP - MINP) / POINTS_PER_ISENTROP as f64;
+            while p < MAXP + 1.0001 * dp {
+                let t = find_root(&|t| {theta_e_saturated_kelvin(p,t)- theta_e_k},
+                    -150.0, 60.0);
+                v.push(TPCoords{temperature:t, pressure: p});
+                p += dp;
+            }
+            v
+        })
+        .collect()
+    };
+}
+
+/// Generate a list of Temperature, Pressure points along an isentrope.
+pub fn generate_isentrop(theta: f64) -> Vec<TPCoords> {
+    use std::f64;
+    use config::{MAXP, ISENTROPS_TOP_P, POINTS_PER_ISENTROP};
+
+    let mut result = vec![];
+
+    let mut p = MAXP;
+    while p >= ISENTROPS_TOP_P {
+        let t = ::formula::temperature_c_from_theta(theta, p);
+        result.push(TPCoords {
+            temperature: t,
+            pressure: p,
+        });
+        p += (ISENTROPS_TOP_P - MAXP) / (POINTS_PER_ISENTROP as f64);
+    }
+    let t = ::formula::temperature_c_from_theta(theta, ISENTROPS_TOP_P);
+    result.push(TPCoords {
+        temperature: t,
+        pressure: ISENTROPS_TOP_P,
+    });
+
+    result
+}
+
--- src/formula.rs
+++ src/formula.rs
@@ -48,3 +48,62 @@ pub fn theta_e_saturated_kelvin(pressure_hpa: f64, temperature_c: f64) -> f64 {
 pub fn celsius_to_f(temperature: f64) -> f64 {
     1.8 * temperature + 32.0
 }
+
+/// Bisection algorithm for finding the root of an equation given values bracketing a root. Used
+/// when drawing moist adiabats.
+pub fn find_root(f: &Fn(f64) -> f64, mut low_val: f64, mut high_val: f64) -> f64 {
+    use std::f64;
+    const MAX_IT: usize = 50;
+    const EPS: f64 = 1.0e-3;
+
+    if low_val > high_val {
+        ::std::mem::swap(&mut low_val, &mut high_val);
+    }
+
+    let mut f_low = f(low_val);
+    // let mut f_high = f(high_val);
+
+    let mut mid_val = (high_val - low_val) / 2.0 + low_val;
+    let mut f_mid = f(mid_val);
+    for _ in 0..MAX_IT {
+        if f_mid * f_low > 0.0 {
+            low_val = mid_val;
+            f_low = f_mid;
+        } else {
+            high_val = mid_val;
+            // f_high = f_mid;
+        }
+
+        if (high_val - low_val).abs() < EPS {
+            break;
+        }
+
+        mid_val = (high_val - low_val) / 2.0 + low_val;
+        f_mid = f(mid_val);
+    }
+
+    mid_val
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::f64;
+
+    fn approx_equal(val1: f64, val2: f64) -> bool {
+        const EPS: f64 = 1.0e-3;
+
+        (val1 - val2).abs() < EPS
+    }
+
+    #[test]
+    fn test_theta_kelvin() {
+        assert!(approx_equal((32.0 + 273.15), theta_kelvin(1000.0, 32.0)));
+    }
+
+    #[test]
+    fn test_find_min() {
+        assert!(approx_equal(1.0, find_root(&|x| x * x - 1.0, 2.0, 0.0)));
+        assert!(approx_equal(-1.0, find_root(&|x| x * x - 1.0, -2.0, 0.0)));
+    }
+}
